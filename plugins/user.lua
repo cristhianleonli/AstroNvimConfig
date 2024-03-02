fafa6f0f7d0febacc97ae12b136dda30e981edfc
Cristhian Leon <cristhian.leon@inspirato.com>
2024-03-02 12:08:35 -0500

    Replaced neotree with nvimtree

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,44 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    event = "User AstroFile",
+    cmd = { "TodoQuickFix" },
+    keys = {
+      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" },
+    }
+  },
+  {
+    "rose-pine/neovim",
+    lazy = false,
+    priority = false,
+    config = function()
+      require("rose-pine").setup({
+        variant = "dawn",
+        styles = {
+          italic = false,
+        }
+      })
+    end
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    enabled = false,
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    lazy = false,
+    config = function()
+      require("nvim-tree").setup({
+        filters = {
+          dotfiles = true,
+        },
+        view = {
+          adaptive_size = true,
+        },
+      })
+    end,
+  },
 }
